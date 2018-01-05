8170e38210a1830863422c12a09d7e50657dff76
EvanRupert <rupertevanr@gmail.com>
2018-01-05 00:29:25 -0500

    Removed todos from mix file

--- mix.exs
+++ mix.exs
@@ -41,26 +41,3 @@ defmodule Telebrew.Mixfile do
     ]
   end
 end
-
-# TODO: write github readme with install, config, and getting started instructions
-
-# TODO: implement all methods in the telegram bot api
-
-# TODO: implement when guard by using macros to define a new guard function
-# and then use that function in the when guard for the generated function
-
-# TODO: test repeat match declarations and stuff like that and then implement validation for that
-
-# TODO: write documentation for all functions
-
-# TODO: maybe add helper functions that will get the last n messages or something
-
-# TODO: write simple response macro without the need of m.chat.id
-# for message sending
-# Example
-# on "photo" do
-#   respond "Received photo"
-# end
-
-# TODO: do some process supervison thing to restart the process
-# that keeps timing out
