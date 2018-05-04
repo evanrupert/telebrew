2884954ae4f0112b712c7553fb590db425b40e09
evanrupert <rupertevanr@gmail.com>
2018-05-03 23:31:02 -0400

    Added mock wrapper test

--- test/polling_test.exs
+++ test/polling_test.exs
@@ -7,4 +7,10 @@ defmodule PollingTest do
     assert 111_111_111 == get_last_update_id()
   end
 
+  test "mock wrapper returns proper update" do
+    assert {:ok, [update|_]} = Telebrew.Test.MockWrapper.get_updates(111_111_110)
+
+    assert update.update_id == 111_111_111
+  end
+
 end
