92c5678193b1d3c9f7d9a8554359c13d8e23696c
evanrupert <rupertevanr@gmail.com>
2018-05-07 19:36:39 -0400

    Removed client side configuration

--- config/dev.exs
+++ config/dev.exs
@@ -1,16 +1,16 @@
 use Mix.Config
 
-config :telebrew,
-  api_key: System.get_env("TELEGRAM_API_KEY")
+# config :telebrew,
+#   api_key: System.get_env("TELEGRAM_API_KEY")
 
-config :telebrew,
-  telegram_wrapper: Nadia
+# config :telebrew,
+#   telegram_wrapper: Nadia
 
-config :telebrew,
-  quiet: false
+# config :telebrew,
+#   quiet: false
 
-config :telebrew,
-  websocket_url: "TODO"
+# config :telebrew,
+#   websocket_url: "TODO"
 
-config :nadia,
-  token: System.get_env("TELEGRAM_API_KEY")
+# config :nadia,
+#   token: System.get_env("TELEGRAM_API_KEY")
--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
-config :telebrew, api_key: System.get_env("TELEGRAM_API_KEY")
+# config :telebrew, api_key: System.get_env("TELEGRAM_API_KEY")
 
-config :nadia, token: System.get_env("TELEGRAM_API_KEY")
+# config :nadia, token: System.get_env("TELEGRAM_API_KEY")
 
 config :telebrew,
   telegram_wrapper: Telebrew.Test.MockWrapper
