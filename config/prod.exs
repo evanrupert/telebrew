cd50dedb66860e3cce4f1fbd19e53e86637dce5d
evanrupert <rupertevanr@gmail.com>
2018-05-12 16:26:23 -0400

    Added webhook configuration to prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,2 +1,11 @@
 use Mix.Config
 
+config :telebrew,
+  telegram_wrapper: Nadia,
+  quiet: false,
+  listener_module: Testing,
+  webhook_url: "https://telebrew-testing.herokuapp.com/"
+
+config :nadia,
+  token: System.get_env("TELEGRAM_API_KEY")
+
