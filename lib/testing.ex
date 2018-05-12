5d33f7ea8b8dcec1ec40d661ea5a87d7a6c7a2c4
evanrupert <rupertevanr@gmail.com>
2018-05-12 13:22:54 -0400

    Removed random testing listeners

--- lib/testing.ex
+++ lib/testing.ex
@@ -6,12 +6,4 @@ defmodule Testing do
 
   on "/date", do: respond m.date
 
-  on "/something" do
-    respond "Doing something"
-  end
-
-  on "/something_else" do
-    respond "Doing something else"
-  end
-
 end
