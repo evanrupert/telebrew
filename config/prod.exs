cf541d19d5f3609e4647e4f668dabd22e06324cc
evanrupert <rupertevanr@gmail.com>
2018-05-12 17:41:53 -0400

    Removed webhook config

--- config/prod.exs
+++ config/prod.exs
@@ -3,8 +3,7 @@ use Mix.Config
 config :telebrew,
   telegram_wrapper: Nadia,
   quiet: false,
-  listener_module: Testing,
-  webhook_url: "https://telebrew-testing.herokuapp.com/"
+  listener_module: Testing
 
 config :nadia,
   token: System.get_env("TELEGRAM_API_KEY")
